053a2ab058ee47f1a1ef52eaebfb95a660cd4bf4
Vasile Grafu <vasilegrafu@gmail.com>
2026-08-06 21:42:35 +0300

    .

--- solution.university/database/base_database_model.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-
-"""------------------------------------------------------------------------------------------------
-"""
-class BaseDatabaseModel(DeclarativeBase):
-    pass
--- solution.university/database/enums/__init__.py
+++
@@ -1,4 +0,0 @@
-from .degree_level import DegreeLevel
-from .student_status import StudentStatus
-from .term_kind import TermKind
-from .enrollment_status import EnrollmentStatus
